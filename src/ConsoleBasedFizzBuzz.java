a914a6d3035264434384e15ec9ae9cc9c1d5e983
Firepants69 <131135211+Firepants69@users.noreply.github.com>
2024-05-23 15:43:25 -0600

    Update ConsoleBasedFizzBuzz.java

--- src/ConsoleBasedFizzBuzz.java
+++ src/ConsoleBasedFizzBuzz.java
@@ -4,13 +4,13 @@ class ConsoleBasedFizzBuzz implements Fizzbuzz{
     public void print(int from, int to){
         for(int i = from;i<to+1;i++){
             if(i%3==0 && i%5==0){
-				System.out.println("FizzBuzz");
-			}else if(i%5==0){
-				System.out.println("Buzz");
-			}else if(i%3==0){
-				System.out.println("Fizz");
-			}else{
-				System.out.println(i);
+		System.out.println("FizzBuzz");
+	    }else if(i%5==0){
+		System.out.println("Buzz");
+	    }else if(i%3==0){
+		System.out.println("Fizz");
+	    }else{
+		System.out.println(i);
 			}
         }
     }
